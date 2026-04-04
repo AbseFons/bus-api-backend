bb70c79dc22a595551f9e06c1c562dc40560e52c
AbseFons <abner.fonseca.melgarejo@gmail.com>
2026-04-03 20:13:35 -0500

    chore: backend service  implemented

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,21 @@
-INSERT INTO brands (name) VALUES ('Volvo');
-INSERT INTO brands (name) VALUES ('Scania');
-INSERT INTO brands (name) VALUES ('Fiat');
+INSERT INTO brands (name) VALUES ('Volvo') ON CONFLICT (name) DO NOTHING;
+INSERT INTO brands (name) VALUES ('Scania') ON CONFLICT (name) DO NOTHING;
+INSERT INTO brands (name) VALUES ('Fiat') ON CONFLICT (name) DO NOTHING;
 
 INSERT INTO buses (bus_number, plate, created_at, features, active, brand_id)
-VALUES ('BUS-001', 'ABC-123', NOW(), 'Aire acondicionado, 2 pisos', true, 1);
+SELECT 'BUS-001', 'ABC-123', NOW(), 'Aire acondicionado, 2 pisos', true, id
+FROM brands
+WHERE name = 'Volvo'
+ON CONFLICT (plate) DO NOTHING;
 
 INSERT INTO buses (bus_number, plate, created_at, features, active, brand_id)
-VALUES ('BUS-002', 'BCD-234', NOW(), 'Asientos reclinables', true, 2);
+SELECT 'BUS-002', 'BCD-234', NOW(), 'Asientos reclinables', true, id
+FROM brands
+WHERE name = 'Scania'
+ON CONFLICT (plate) DO NOTHING;
 
 INSERT INTO buses (bus_number, plate, created_at, features, active, brand_id)
-VALUES ('BUS-003', 'CDE-345', NOW(), 'WiFi y puerto USB', false, 3);
+SELECT 'BUS-003', 'CDE-345', NOW(), 'WiFi y puerto USB', false, id
+FROM brands
+WHERE name = 'Fiat'
+ON CONFLICT (plate) DO NOTHING;
